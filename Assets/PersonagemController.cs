8d3685450fe83beb29055353bcc5f1e19ee0af36
Olavo Menezes Gouvêa <olavotf3@gmail.com>
2026-06-05 15:08:29 -0300

    pulo pela metade

--- Assets/PersonagemController.cs
+++ Assets/PersonagemController.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class PersonagemController : MonoBehaviour
 {
- public Rigidbody2D rb2d;
-
-public float vel;
+    public Rigidbody2D rb2d; 
+    public float vel;
+    public float jumpForce;
+    public GameObject GroundCheck;
+    // Start is called before the first frame update
     void Start()
     {
-    rb2d = this.GetComponent<Rigidbody2D>();   
+        rb2d = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal");
         if(rb2d.velocity.magnitude < 5){
-        rb2d.velocity += new Vector2(vel,0)*horizontalInput*Time.deltaTime;
-    }
+        rb2d.velocity += new Vector2(vel,0) *moveHorizontal * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.Space)){
+            rb2d.velocity = new Vector2(rb2d.velocity.x,jumpForce);
+        }
     }
 }
